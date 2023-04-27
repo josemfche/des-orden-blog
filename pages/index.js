a3d7d5a071f8b36106abcee9a39c7a6eb74abe55
jose martinez <che.manuel758@gmail.com>
2023-04-27 14:59:08 -0400

    categories selector

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
+import { useState } from 'react';
 import { FeaturedPosts } from '../sections/index';
-import { PostCard, Categories, PostWidget, Banner } from '../components';
+import { PostCard, Categories, PostWidget, Banner, CategoriesBar } from '../components';
 import { getPosts } from '../services';
 
 export default function Home({ posts }) {
+  const [categorySlug, setCategorySlug] = useState('all');
+
+  // eslint-disable-next-line no-nested-ternary
+  const filteredPosts = categorySlug === 'all'
+    ? posts
+    : posts.filter((post) => post.node.categories.some(
+      (category) => category.slug === categorySlug,
+    ));
+
   return (
     <div className="container mx-auto px-1 mb-8">
       <Banner />
+      <CategoriesBar setCategorySlug={setCategorySlug} />
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
+          {filteredPosts.map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
         </div>
