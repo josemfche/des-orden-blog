a119c3ea6196de44a42b2d4303dea866e07cbc7d
jose martinez <che.manuel758@gmail.com>
2023-05-01 20:25:26 -0400

    update banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
 
 const Hero = () => (
   <div className="container mx-auto my-10 ">
     <div className="px-6 lg:px-16 py-10 rounded-3xl">
       <div className="flex flex-col lg:flex-row justify-center  lg:justify-between items-center gap-5">
-        <div className="flex flex-col gap-5 lg:gap-10">
-          <p className=" text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-left">
-            Economía, política e inversiones desde otra perspectiva.
-          </p>
-          <p className="text-sm sm:text-base lg:text-lg text-gray-500">
-            Soy venezolano, médico psiquiatra dedicado a las finanzas, intentando dar explicaciones al des-orden que vivimos.
-          </p>
+        <div className="flex flex-row gap-5 lg:gap-10">
+          <div className="flex flex-col gap-5 lg:gap-10">
+            <p className=" text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-left">
+              Economía, política e inversiones desde otra perspectiva.
+            </p>
+            <p className="text-sm sm:text-base lg:text-lg text-gray-500">
+              Soy venezolano, médico psiquiatra dedicado a las finanzas, intentando dar explicaciones al des-orden que vivimos.
+            </p>
+          </div>
+          <div className="">
+            <Image width="500" height="400" className="rounded-lg" src="/frame49.png" />
+          </div>
         </div>
-        {/*         <img className="max-w-md h-1/4 rounded-lg" src="https://github.com/AbelinCoin/Blog-Des-orden/blob/main/Frame%2049.png?raw=true" alt="img" /> */}
       </div>
     </div>
     <div className="flex items-center justify-center">
