5309eb36be67de2753cb344a6efb769664f657ae
jose martinez <che.manuel758@gmail.com>
2023-07-31 22:08:35 -0400

    footer header

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,15 @@
-import Image from 'next/image';
+/* import Image from 'next/image'; */
 import Link from 'next/link';
 import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => (
   <footer className="bg-thegray">
-    <div className="container mx-auto py-12 px-8 sm:px-24 flex md:flex-row flex-col justify-between sm:flex-row border-b boder-white items-center">
+    <div className="container mx-auto py-12 px-8 sm:px-24 flex md:flex-row flex-col justify-between sm:flex-row border-b boder-white items-center h-44">
       <div className="flex-grow sm:flex-grow-0 flex justify-center sm:justify-start mb-4 sm:mb-0">
         <Link href="/">
           <div className="flex cursor-pointer items-center">
-            <Image width={100} height={100} src="/logoBlog.png" alt="Logo" />
+            {/*             <Image width={100} height={100} src="/logoBlog.png" alt="Logo" /> */}
             <span className="font-bold text-2xl text-white">Des-orden</span>
           </div>
         </Link>
@@ -52,13 +52,13 @@ const Footer = () => (
     </div>
     <div className="flex justify-center w-full">
       <ul className="flex justify-center items-center sm:justify-start text-white h-32">
-        <li className="mx-5">
+        <li className="mx-5 underline">
           <a href="/">Politicas de Privacidad</a>
         </li>
-        <li className="mx-5">
+        <li className="mx-5 underline">
           <a href="/">Terminos de Servicio</a>
         </li>
-        <li className="mx-5">
+        <li className="mx-5 underline">
           <a href="/">Configuraciones de Cookies</a>
         </li>
       </ul>
--- components/Header.jsx
+++ components/Header.jsx
@@ -82,7 +82,7 @@ const Header = () => {
           <input
             type="text"
             placeholder="Buscar"
-            className="searchInputHome text-gray-400 bg-gray-100 py-2 pl-4 pr-12 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-400 focus:border-transparent"
+            className="searchInputHome border text-gray-400 bg-white py-2 pl-4 pr-12 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-400 focus:border-transparent"
           />
           <div className="absolute inset-y-0 right-0 flex items-center mr-3 pointer-events-none">
             <FontAwesomeIcon icon={faSearch} className="text-black" />
